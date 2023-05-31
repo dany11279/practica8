6b402368c00bf1ee2ecd3f66545f2e3868e1afe9
Dany <danymel1279@gmail.com>
2023-05-31 10:05:30 -0600

    realice cambios en la validacion del nit

--- modelos/Cliente.php
+++ modelos/Cliente.php
@@ -19,18 +19,22 @@ class Cliente extends Conexion{
     public function guardar(){
         // Validar el NIT antes de guardar los datos
         if (!$this->validarNit($this->cliente_nit)) {
-            echo "El NIT ingresado es inválido. No se guardarán los datos.";
+            echo  "<h3>El NIT ingresado es inválido. No se guardarán los datos.</h3>";
+
             // Detener la ejecución del código o redirigir a otra página, según sea necesario
             exit();
         }
+
+        
+        
     
         $sql = "INSERT INTO clientes (cliente_nombre, cliente_nit) VALUES ('$this->cliente_nombre','$this->cliente_nit')";
         $resultado = self::ejecutar($sql);
     
         if ($resultado) {
-            echo "Datos guardados correctamente. El NIT es válido.";
+            echo "<h3>Datos guardados correctamente. El NIT es válido.</h3>";
         } else {
-            echo "Error al guardar los datos.";
+            echo "<h3>Error al guardar los datos.</h3>";
         }
         
         return $resultado;
@@ -99,3 +103,4 @@ class Cliente extends Conexion{
     
     
 }
+
--- vistas/clientes/index.php
+++ vistas/clientes/index.php
@@ -13,7 +13,7 @@
                 <div class="row mb-3">
                     <div class="col">
                         <label for="cliente_nit">Nit del Cliente</label>
-                        <input type="number" step="0.01" min="0" name="cliente_nit" id="cliente_nit" class="form-control">
+                        <input type="text"  min="0" name="cliente_nit" id="cliente_nit" class="form-control">
                     </div>
                 </div>
                 <div class="row mb-3">
